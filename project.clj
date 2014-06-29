812d93a0a46b4b0a20aba0d1ae68da0c7f92018b
Michael Sappler <michaelsappler@gmail.com>
2014-06-29 16:20:32 +0200

    Release 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject grid2d "0.1.0-SNAPSHOT"
+(defproject grid2d "0.1.0"
   :description "Two dimensional grid data structure"
   :url "https://github.com/damn/grid2d"
   :license {:name "MIT"}
