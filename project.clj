745401ab1ff8fe843fc191e60f8301971daabeeb
Michael Sappler <msappler@sixreasons.de>
2015-07-21 14:35:58 +0200

    Update readme and project

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject grid2d "0.1.0"
+(defproject grid2d "0.1.0-SNAPSHOT"
   :description "Two dimensional grid data structure"
   :url "https://github.com/damn/grid2d"
   :license {:name "MIT"}
